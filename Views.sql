27e6278a57c59a8ca4dcb40d3cac913cb957bc96
LivinkumarS <001liv100@ggmail.com>
2025-10-17 12:03:03 +0530

    Views

--- Views.sql
+++ Views.sql
@@ -12,9 +12,20 @@ insert Into student (name,age,email,phone) values
 ('anbu',23,'anbu@gmail.com',6776898977);
 
 select * from student;
+select * from gender;
 
 select id,email,phone from student;
 
 create view contact_details as select id,email,phone from student;
 
 select * from contact_details;
+
+create table gender (
+id  int primary key auto_increment,
+gender enum ("male","female") not null,
+studentId int not null,
+foreign key (studentId) references student(id)
+
+);
+
+insert into gender (gender,studentId) values ('male',1);
